97a5ffc752ac38fe1f5a4a7b5ac508d1b5a66b24
Gyumin Kim <starkying@gmail.com>
2021-03-19 19:51:58 +0900

    (ch1) `UserDao`를 setter 주입 방식으로 변경

--- ch1/src/main/java/user/dao/CountingDaoFactory.java
+++ ch1/src/main/java/user/dao/CountingDaoFactory.java
@@ -8,7 +8,9 @@ public class CountingDaoFactory {
 
     @Bean
     public UserDao userDao() {
-        return new UserDao(connectionMaker());
+        UserDao userDao = new UserDao();
+        userDao.setConnectionMaker(connectionMaker());
+        return userDao;
         // 추후 UserDao가 아닌 다른 종류의 DAO가 필요해지면, 위와 동일한 방식으로 DI를 해주면 손쉽게 대응이 가능하다.
     }
 
