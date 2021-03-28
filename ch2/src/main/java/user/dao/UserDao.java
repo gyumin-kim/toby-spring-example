b9388d30ece62d280d529ff73f4351f64f80d7d2
Gyumin Kim <starkying@gmail.com>
2021-03-28 14:27:04 +0900

    (ch2) 2장 모듈 생성
    * 1장 마지막 부분까지의 코드를 옮김

---
+++ ch2/src/main/java/user/dao/UserDao.java
@@ -0,0 +1,53 @@
+package user.dao;
+
+import user.domain.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDao {
+
+    private ConnectionMaker connectionMaker;
+
+    public void setConnectionMaker(final ConnectionMaker connectionMaker) {
+        this.connectionMaker = connectionMaker;
+    }
+
+    public void add(final User user) throws ClassNotFoundException, SQLException {
+        // DB 연결을 위한 Connection을 가져온다.
+        Connection c = connectionMaker.makeNewConnection();
+        // SQL을 담은 Statement(또는 PreparedStatement)를 만든다.
+        PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+        // 만들어진 Statement를 실행한다.
+        ps.executeUpdate();
+        // 작업 중에 생성된 리소스는 작업을 마친 후 반드시 닫아준다.
+        ps.close();
+        c.close();
+    }
+
+    public User get(final String id) throws ClassNotFoundException, SQLException {
+        // DB 연결을 위한 Connection을 가져온다.
+        Connection c = connectionMaker.makeNewConnection();
+        // SQL을 담은 Statement(또는 PreparedStatement)를 만든다.
+        PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
+        ps.setString(1, id);
+        // (조회) SQL 쿼리의 실행 결과를 ResultSet으로 받아서 User 오브젝트에 옮겨준다.
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+        // 작업 중에 생성된 리소스는 작업을 마친 후 반드시 닫아준다.
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+}
---
+++ ch2/src/main/java/user/dao/UserDaoTest.java
@@ -0,0 +1,29 @@
+package user.dao;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import user.domain.User;
+
+import java.sql.SQLException;
+
+public class UserDaoTest {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        User user = new User();
+        user.setId("gyumin-kim");
+        user.setName("김규민");
+        user.setPassword("1234");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회 성공");
+    }
+}
