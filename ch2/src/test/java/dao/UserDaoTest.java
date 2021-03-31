985fa5f70359412ccadc89c1e7b9452c2dd97de8
Gyumin Kim <starkying@gmail.com>
2021-04-01 02:58:51 +0900

    (ch2) AssertJ 적용

--- ch2/src/test/java/dao/UserDaoTest.java
+++ ch2/src/test/java/dao/UserDaoTest.java
@@ -8,7 +8,7 @@ import user.domain.User;
 
 import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserDaoTest {
 
@@ -18,7 +18,7 @@ public class UserDaoTest {
         UserDao dao = context.getBean("userDao", UserDao.class);
 
         dao.deleteAll();
-        assertEquals(0, dao.getCount());
+        assertThat(dao.getCount()).isEqualTo(0);
 
         User user = new User();
         user.setId("gyumin-kim");
@@ -26,11 +26,11 @@ public class UserDaoTest {
         user.setPassword("1234");
 
         dao.add(user);
-        assertEquals(1, dao.getCount());
+        assertThat(dao.getCount()).isEqualTo(1);
 
         User user2 = dao.get(user.getId());
 
-        assertEquals(user.getName(), user2.getName());
-        assertEquals(user.getPassword(), user2.getPassword());
+        assertThat(user.getName()).isEqualTo(user2.getName());
+        assertThat(user.getPassword()).isEqualTo(user2.getPassword());
     }
 }
