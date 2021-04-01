1d14f17635fbbe906dc42e521c286245b0bbb223
Gyumin Kim <starkying@gmail.com>
2021-04-01 20:00:25 +0900

    (ch2) getCount() 테스트 추가
    * `User` 생성자 추가

--- ch2/src/test/java/dao/UserDaoTest.java
+++ ch2/src/test/java/dao/UserDaoTest.java
@@ -20,10 +20,7 @@ public class UserDaoTest {
         dao.deleteAll();
         assertThat(dao.getCount()).isEqualTo(0);
 
-        User user = new User();
-        user.setId("gyumin-kim");
-        user.setName("김규민");
-        user.setPassword("1234");
+        User user = new User("gyumin-kim", "김규민", "1234");
 
         dao.add(user);
         assertThat(dao.getCount()).isEqualTo(1);
@@ -32,5 +29,27 @@ public class UserDaoTest {
 
         assertThat(user.getName()).isEqualTo(user2.getName());
         assertThat(user.getPassword()).isEqualTo(user2.getPassword());
+    }
+
+    @Test
+    void count() throws SQLException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        User user1 = new User("gyumin1", "김규민", "springno1");
+        User user2 = new User("gyumin2", "김규민", "springno2");
+        User user3 = new User("gyumin3", "김규민", "springno3");
+
+        dao.deleteAll();
+        assertThat(dao.getCount()).isEqualTo(0);
+
+        dao.add(user1);
+        assertThat(dao.getCount()).isEqualTo(1);
+
+        dao.add(user2);
+        assertThat(dao.getCount()).isEqualTo(2);
+
+        dao.add(user3);
+        assertThat(dao.getCount()).isEqualTo(3);
     }
 }
