d3c65c26042cc91f4fbf41f54f9ca7c8f51f47a9
Gyumin Kim <starkying@gmail.com>
2021-04-07 00:48:48 +0900

    (ch3) `UserDao`에서 context와 client를 분리(`deleteAll()`만)
    * context는 `jdbcContextWithStatementStrategy()`, client는 `deleteAll()`

--- ch3/src/main/java/user/dao/UserDao.java
+++ ch3/src/main/java/user/dao/UserDao.java
@@ -17,6 +17,32 @@ public class UserDao {
         this.dataSource = dataSource;
     }
 
+    public void jdbcContextWithStatementStrategy(final StatementStrategy stmt) throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+
+        try {
+            c = dataSource.getConnection();
+            ps = stmt.makePreparedStatement(c);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                }
+            }
+        }
+    }
+
     public void add(final User user) throws SQLException {
         Connection c = dataSource.getConnection();
 
@@ -59,13 +85,8 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        Connection c = dataSource.getConnection();
-
-        PreparedStatement ps = c.prepareStatement("delete from users");
-        ps.executeUpdate();
-
-        ps.close();
-        c.close();
+        StatementStrategy st = new DeleteAllStatement();
+        jdbcContextWithStatementStrategy(st);
     }
 
     public int getCount() throws SQLException {
