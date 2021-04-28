a6245c088733b4871c6b789277572737bc623dca
Gyumin Kim <starkying@gmail.com>
2021-04-29 00:03:06 +0900

    (ch3) 메서드 추출로 템플릿 설계, 콜백 인터페이스(`BufferedReaderCallback`) 추가
    * 템플릿: 파일을 열고 각 라인을 읽어올 수 있는 BufferedReader를 만듦
    * 콜백: 작업을 처리한 후에 최종 결과만 템플릿에게 전달

--- ch3/src/main/java/learningtest/template/Calculator.java
+++ ch3/src/main/java/learningtest/template/Calculator.java
@@ -6,14 +6,36 @@ import java.io.IOException;
 
 public class Calculator {
     public int calcSum(final String filePath) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(filePath));
-        int sum = 0;
-        String line;
-        while ((line = br.readLine()) != null) {
-            sum += Integer.parseInt(line);
-        }
+        BufferedReaderCallback sumCallback = new BufferedReaderCallback() {
+            @Override
+            public int doSomethingWithReader(final BufferedReader br) throws IOException {
+                int sum = 0;
+                String line;
+                while ((line = br.readLine()) != null) {
+                    sum += Integer.parseInt(line);
+                }
+                return sum;
+            }
+        };
+        return fileReadTemplate(filePath, sumCallback);
+    }
 
-        br.close();
-        return sum;
+    public int fileReadTemplate(final String filepath, final BufferedReaderCallback callback) throws IOException {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(filepath));
+            return callback.doSomethingWithReader(br);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
     }
 }
